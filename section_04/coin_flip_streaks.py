fa83fc79ef9cb723b128a4a2b56c4fb2917e329a
sicou2 <sicou2@gmail.com>
2020-05-03 18:47:53 -0500

    complete section 4 and start 5

--- section_04/coin_flip_streaks.py
+++ section_04/coin_flip_streaks.py
@@ -1,15 +1,30 @@
 import random
 
 number_of_streaks = 0
+trials = 10_000_000
+coin = ['heads', 'tails']
+#six_flips = ''
+#data = []
+for experimentNumber in range(trials):
 
-for experimentNumber in range(10000):
+    six_flips = ''
+    for trial in range(6):
+        six_flips += f"{random.choice(coin)} "
 
-    hundred_flips = []
-test = [1,2,3]
+#    if ('heads heads heads heads heads heads' or 'tails tails tails tails tails tails') in six_flips:
+    if 'heads heads heads heads heads heads' in six_flips or 'tails tails tails tails tails tails' in six_flips:
+        number_of_streaks += 1
+#    if 'tails tails tails tails tails tails' in six_flips:
+#        number_of_streaks += 1
+#    data.append(six_flips)
 
-if [1,2,3] in test:
-    print(True)
+#for i in data:
+ #   if 'heads heads heads heads heads heads' in i:
+  #      number_of_streaks += 1
 
+#print(data)
+print(number_of_streaks)
+print((number_of_streaks/trials)*100)
 
 
 
